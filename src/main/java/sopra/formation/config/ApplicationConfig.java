2da88f4c6be2065edcc69dbab33d665ea4125173
Duchene27u <benjamin.duchene3@gmail.com>
2022-01-19 16:34:18 +0100

    Ajout de la structure template

--- src/main/java/sopra/formation/config/ApplicationConfig.java
+++ src/main/java/sopra/formation/config/ApplicationConfig.java
@@ -66,7 +66,7 @@ public class ApplicationConfig {
 
 	private Properties hibernateProperties() {
 		Properties properties = new Properties();
-		properties.setProperty("hibernate.hbm2ddl.auto", "create");
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		properties.setProperty("hibernate.dialect", env.getProperty("db.dialect"));
 		properties.setProperty("hibernate.show_sql", "true");
 
