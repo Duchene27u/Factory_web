00eebf5482c68a5f27e7a72e04179b59b45d2a49
Duchene27u <benjamin.duchene3@gmail.com>
2022-01-19 15:49:12 +0100

    First commit

---
+++ src/main/java/sopra/formation/config/WebConfig.java
@@ -0,0 +1,64 @@
+package sopra.formation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@Configuration
+@EnableWebMvc // Activation des annotations @Controller, @RequestMapping, ...
+@ComponentScan("sopra.formation.web") // Scanner le package où se situe les Controller
+public class WebConfig implements WebMvcConfigurer {
+
+	// Configuration des exclusions sur les urls ...../css/** vers un répertoire
+	// physique
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+	}
+
+	// Configure le chemin des templates Thymeleaf
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+
+		return templateResolver;
+	}
+
+	// Configure le moteur Thymeleaf en lui précision le templateResolver et des
+	// options supplémentaires
+	@Bean
+	public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		templateEngine.setEnableSpringELCompiler(true);
+		templateEngine.addDialect(new Java8TimeDialect());
+		templateEngine.addDialect(new LayoutDialect());
+
+		return templateEngine;
+	}
+
+	// Permet à Spring MVC de savoir quel moteur de rendu (View) utilisé
+	// En remplacement du ViewResolver standard en JSP/JSTL
+	@Bean
+	public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine);
+
+		return viewResolver;
+	}
+
+}
---
+++ src/main/java/sopra/formation/web/HomeController.java
@@ -0,0 +1,27 @@
+package sopra.formation.web;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("")
+	public String defaut() {
+		return "redirect:/home";
+	}
+
+	@GetMapping("/home")
+	public String home(Model model, @RequestParam(required = false) String username) {
+		
+		model.addAttribute("utilisateur", username);
+		model.addAttribute("dtJour", LocalDate.now());
+		
+		return "home";
+	}
+
+}
